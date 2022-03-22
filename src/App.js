72588fe3d82ee7f1d2770c09e9dbc70beba8e02a
ZaidKhan144 <zaidirfan.945@gmail.com>
2022-03-21 21:02:42 -0500

    display results from api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react'
 
-function App() {
+const App = () => {
+
+  const [ trains, setTrains ] = useState([])
+
+  const fetchTrains = () => {
+
+    let api = 'https://api.wmata.com/TrainPositions/TrainPositions?contentType=json&api_key=29fb1946737f4e1a8d20d620cdcb02d8'
+
+    fetch(api)
+    .then(response => response.json())
+    .then(data => setTrains(data.TrainPositions))
+    .catch(error => {
+      console.log('Error fetching and parsing data', error)
+    })
+  }
+
+  useEffect(() => {
+    fetchTrains()
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="section">
+        <div className="trains">
+          <h1>Trains</h1>
+          <div className="card-container">
+            {
+              trains.map((item, index) => {
+                return (
+                  <div className="card" key={index}>
+                    <p> <span>Train Id:</span> {item.TrainId}</p>
+                    <p> <span>Train Number:</span> {item.TrainNumber}</p>
+                    <p> <span>Car Count:</span> {item.CarCount}</p>
+                    <p> <span>Direction Num:</span> {item.DirectionNum}</p>
+                    <p> <span>Circuit Id:</span> {item.CircuitId}</p>
+                    <p> <span>Destination Station Code:</span> {item.DestinationStationCode}</p>
+                    <p> <span>Line Code:</span> {item.LineCode}</p>
+                    <p> <span>Seconds At Location:</span> {item.SecondsAtLocation}</p>
+                    <p> <span>Service Type:</span> {item.ServiceType}</p>
+                  </div>
+                )
+              })
+            }
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
